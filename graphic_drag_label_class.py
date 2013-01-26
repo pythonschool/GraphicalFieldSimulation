48a58cef0644baedfc3cabf4f36af1009877df1a
Adam McNicol <adamsorders@mac.com>
2013-01-26 20:08:16 +0000

    Graphic drag label mouse move event added
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- graphic_drag_label_class.py
+++ graphic_drag_label_class.py
@@ -1,4 +1,5 @@
-from PyQt4.QtGui import QLabel, QPixmap
+from PyQt4.QtGui import QLabel, QPixmap, QDrag
+from PyQt4.QtCore import *
 
 import field_resources
 
@@ -10,22 +11,37 @@ class QDragLabel(QLabel):
 		super().__init__()
 		self.setPixmap(picture.scaledToWidth(25,1))
 
+	def mouseMoveEvent(self,event):
+		#if the left mouse button is used
+		if event.buttons() == Qt.LeftButton:
+			data = QByteArray()
+			mime_data = QMimeData()
+			mime_data.setData(self.mimetext,data)
+			drag = QDrag(self)
+			drag.setMimeData(mime_data)
+			drag.setHotSpot(self.rect().topLeft()) #where do we drag from
+			drop_action = drag.start(Qt.MoveAction) #drag starts
+
 class WheatDragLabel(QDragLabel):
 	"""this class provides an wheat label that can be dragged and dropped"""
 	def __init__(self):
 		super().__init__(QPixmap(":/wheat_seed.png"))
+		self.mimetext = "application/x-wheat"
 
 class PotatoDragLabel(QDragLabel):
 	"""this class provides an potato label that can be dragged and dropped"""
 	def __init__(self):
 		super().__init__(QPixmap(":/potato_seed.png"))
+		self.mimetext = "application/x-potato"
 
 class CowDragLabel(QDragLabel):
 	"""this class provides an cow label that can be dragged and dropped"""
 	def __init__(self):
 		super().__init__(QPixmap(":/cow_baby.png"))
+		self.mimetext = "application/x-cow"
 
 class SheepDragLabel(QDragLabel):
 	"""this class provides an sheep label that can be dragged and dropped"""
 	def __init__(self):
 		super().__init__(QPixmap(":/sheep_baby.png"))
+		self.mimetext = "application/x-sheep"
