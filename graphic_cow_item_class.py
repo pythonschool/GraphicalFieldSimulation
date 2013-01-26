910a23ccaf39beaa8c01a2c52ac7f88fcc904498
Adam McNicol <adamsorders@mac.com>
2013-01-26 16:59:56 +0000

    Cow and sheep graphic item sub-classes
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ graphic_cow_item_class.py
@@ -0,0 +1,14 @@
+from graphic_animal_class import *
+from cow_class import *
+
+import field_resources
+
+class CowGraphicsPixmapItem(AnimalGraphicsPixmapItem):
+	"""this class provides a graphical representation of a cow"""
+
+	#constructor
+	def __init__(self):
+		self.available_graphics = [":/cow_baby.png", ":/cow_poor.png",":/cow_fine.png",":/cow_prime.png"]
+		super().__init__(self.available_graphics)
+
+		self.animal = Cow("")
