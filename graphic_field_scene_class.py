491fb1bb7dab93eb373e22c096eabab6a7a05cd4
Adam McNicol <adamsorders@mac.com>
2013-01-26 20:17:27 +0000

    Improved drop event method to actually add crops/animals to field
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- graphic_field_scene_class.py
+++ graphic_field_scene_class.py
@@ -22,6 +22,40 @@ class FieldGraphicsScene(QGraphicsScene):
 		self.background_brush.setTexture(self.background_picture)
 		self.setBackgroundBrush(self.background_brush)
 
+	def _drop_position(self,item):
+		cursor_position = QCursor.pos() #global cursor position
+		current_view = self.views()[0]
+		scene_position = current_view.mapFromGlobal(cursor_position)
+
+		width = item.boundingRect().width()
+		height = item.boundingRect().height()
+
+		width_offset = width/2
+		height_offset = height/2
+
+		drop_x = scene_position.x() - width_offset
+		drop_y = scene_position.y() - height_offset
+
+		return drop_x, drop_y
+
+	def _visualise_graphic_item(self,graphic_item_type):
+		if graphic_item_type == "crop":
+			x, y = self._drop_position(self.field._crops[-1])
+			self.field._crops[-1].setPos(x,y)
+			self.addItem(self.field._crops[-1])
+		elif graphic_item_type == "animal":
+			x, y = self._drop_position(self.field._animals[-1])
+			self.field._animals[-1].setPos(x,y)
+			self.addItem(self.field._animals[-1])
+
+	def _add_graphic_item(self,result,graphic_item_type):
+		if result:
+			self._visualise_graphic_item(graphic_item_type)
+		else:
+			error_message = QMessageBox()
+			error_message.setText("No more {0}s can be added to this field".format(graphic_item_type))
+			error_message.exec()
+
 	#this method overrides the parent method
 	def dragEnterEvent(self,event):
 		#what to do if an object is dragged into the scene
@@ -37,12 +71,16 @@ class FieldGraphicsScene(QGraphicsScene):
 
 		#what to do if an object is dropped on the scene
 		if event.mimeData().hasFormat("application/x-wheat"):
-			print("wheat")
+			crop_added = self.field.plant_crop(WheatGraphicsPixmapItem())
+			self._add_graphic_item(crop_added,"crop")
 		elif event.mimeData().hasFormat("application/x-potato"):
-			print("potato")
+			crop_added = self.field.plant_crop(PotatoGraphicsPixmapItem())
+			self._add_graphic_item(crop_added,"crop")
 		elif event.mimeData().hasFormat("application/x-cow"):
-			print("cow")
+			animal_added = self.field.add_animal(CowGraphicsPixmapItem())
+			self._add_graphic_item(animal_added,"animal")
 		elif event.mimeData().hasFormat("application/x-sheep"):
-			print("sheep")
+			animal_added = self.field.add_animal(SheepGraphicsPixmapItem())
+			self._add_graphic_item(animal_added,"animal")
 		else:
 			print("not recognised")
