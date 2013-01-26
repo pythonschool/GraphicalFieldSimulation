e95376d15bfbd832f44bc730977ef71a0fc8d051
Adam McNicol <adamsorders@mac.com>
2013-01-26 16:41:25 +0000

    Field item class added
    this is the base class for all of the graphical items which will be added to the field.
    
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ graphic_field_item_class.py
@@ -0,0 +1,15 @@
+from PyQt4.QtGui import QPixmap, QGraphicsPixmapItem, QGraphicsItem
+
+class FieldItemGraphicsPixmapItem(QGraphicsPixmapItem):
+	"""this class provides a pixmap item with a preset image for the item"""
+
+	#constructor
+	def __init__(self, graphics_list):
+		super().__init__()
+		self.available_graphics = graphics_list
+		self.current_graphic = QPixmap(self.available_graphics[0])
+		self.setPixmap(self.current_graphic.scaledToWidth(25,1))
+		self.setFlag(QGraphicsItem.ItemIsMovable) #allow us to move the graphic in the scene
+
+	def update_status(self):
+		pass
