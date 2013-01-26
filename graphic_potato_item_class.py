ec97ce7f8ac80cff89748b671fb53373fdf9c62d
Adam McNicol <adamsorders@mac.com>
2013-01-26 16:56:33 +0000

    Wheat and potato graphic item sub-classes added
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ graphic_potato_item_class.py
@@ -0,0 +1,14 @@
+from graphic_crop_class import *
+from potato_class import *
+
+import field_resources
+
+class PotatoGraphicsPixmapItem(CropGraphicsPixmapItem):
+	"""this class provides a graphical representation of a potato"""
+
+	#constructor
+	def __init__(self):
+		self.available_graphics = [":/potato_seed.png", ":/potato_seedling.png",":/potato_young.png",":/potato_mature.png",":/potato_old.png"]
+		super().__init__(self.available_graphics)
+
+		self.crop = Potato()
---
+++ graphic_wheat_item_class.py
@@ -0,0 +1,14 @@
+from graphic_crop_class import *
+from wheat_class import *
+
+import field_resources
+
+class WheatGraphicsPixmapItem(CropGraphicsPixmapItem):
+	"""this class provides a graphical representation of a wheat"""
+
+	#constructor
+	def __init__(self):
+		self.available_graphics = [":/wheat_seed.png", ":/wheat_seedling.png",":/wheat_young.png",":/wheat_mature.png",":/wheat_old.png"]
+		super().__init__(self.available_graphics)
+
+		self.crop = Wheat()
