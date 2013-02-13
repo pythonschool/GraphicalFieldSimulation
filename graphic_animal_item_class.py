8f64d5a5c032966ef1ff7cc5b2123d77b81c530e
Adam McNicol <adammcnicol@mac.com>
2013-02-13 14:41:03 +0000

    updated resources file
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_animal_item_class.py
+++ graphic_animal_item_class.py
@@ -6,19 +6,19 @@ class AnimalGraphicsPixmapItem(FieldItemGraphicsPixmapItem):
 	#constructor
 	def __init__(self, graphics_list):
 		super().__init__(graphics_list)
-		self.setPixmap(self.current_graphic.scaledToWidth(50,1))
+		self.setPixmap(self.current_graphic.scaledToWidth(80,1))
 
 		self.animal = None
 
 	def update_status(self):
 		if self.animal._status == "Baby":
-			self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(50,1))
+			self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(80,1))
 		elif self.animal._status == "Poor":
-			self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(50,1))
+			self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(80,1))
 		elif self.animal._status == "Fine":
-			self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(50,1))
+			self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(80,1))
 		elif self.animal._status == "Prime":
-			self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(50,1))
+			self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(80,1))
 
 	def needs(self):
 		return self.animal.needs()
