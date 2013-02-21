58d0f171f19655d8de7c2754623ffbb628552cd6
Adam McNicol <adamsorders@mac.com>
2013-02-21 17:00:00 +0000

    added setup file for cx_freeze
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ setup.py
@@ -0,0 +1,31 @@
+# A simple setup script to create an executable using PyQt4. This also
+# demonstrates the method for creating a Windows executable that does not have
+# an associated console.
+#
+# PyQt4app.py is a very simple type of PyQt4 application
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the application
+
+application_title = "Field Simulation" #what you want to application to be called
+main_python_file = "field_simulation.py" #the name of the python file you use to run the program
+
+import sys
+
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+includes = ["atexit","re"]
+
+setup(
+        name = application_title,
+        version = "0.1",
+        description = "Sample cx_Freeze PyQt4 script",
+        options = {"build_exe" : {"includes" : includes }},
+        executables = [Executable(main_python_file, base = base)])
+
