2070bae4f9d8d3c43819c4c08c27b7b058500a1a
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:16:04 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_field_scene_class.py
+++ graphic_field_scene_class.py
@@ -9,40 +9,40 @@ from graphic_sheep_item_class import *
 import field_resources
 
 class FieldGraphicsScene(QGraphicsScene):
-	"""this class provides a scene to manage items in the field"""
+    """this class provides a scene to manage items in the field"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		super().__init__()
-		
-		self.field = Field(max_crops,max_animals)
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        super().__init__()
+        
+        self.field = Field(max_crops,max_animals)
 
-		self.background_brush = QBrush()
-		self.background_picture = QPixmap(":/field_background.png")
-		self.background_brush.setTexture(self.background_picture)
-		self.setBackgroundBrush(self.background_brush)
+        self.background_brush = QBrush()
+        self.background_picture = QPixmap(":/field_background.png")
+        self.background_brush.setTexture(self.background_picture)
+        self.setBackgroundBrush(self.background_brush)
 
-	#this method overrides the parent method
-	def dragEnterEvent(self,event):
-		#what to do if an object is dragged into the scene
-		event.accept()
+    #this method overrides the parent method
+    def dragEnterEvent(self,event):
+        #what to do if an object is dragged into the scene
+        event.accept()
 
-	#this method overrides the parent method
-	def dragMoveEvent(self,event):
-		event.accept()
+    #this method overrides the parent method
+    def dragMoveEvent(self,event):
+        event.accept()
 
-	#this method overrides the parent method
-	def dropEvent(self,event):
-		event.accept()
+    #this method overrides the parent method
+    def dropEvent(self,event):
+        event.accept()
 
-		#what to do if an object is dropped on the scene
-		if event.mimeData().hasFormat("application/x-wheat"):
-			print("wheat")
-		elif event.mimeData().hasFormat("application/x-potato"):
-			print("potato")
-		elif event.mimeData().hasFormat("application/x-cow"):
-			print("cow")
-		elif event.mimeData().hasFormat("application/x-sheep"):
-			print("sheep")
-		else:
-			print("not recognised")
+        #what to do if an object is dropped on the scene
+        if event.mimeData().hasFormat("application/x-wheat"):
+            print("wheat")
+        elif event.mimeData().hasFormat("application/x-potato"):
+            print("potato")
+        elif event.mimeData().hasFormat("application/x-cow"):
+            print("cow")
+        elif event.mimeData().hasFormat("application/x-sheep"):
+            print("sheep")
+        else:
+            print("not recognised")
