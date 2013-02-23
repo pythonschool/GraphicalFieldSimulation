9194abc6ef2d00644e822ec83365569b337b5cbd
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:05:44 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_field_scene_class.py
+++ graphic_field_scene_class.py
@@ -9,15 +9,15 @@ from graphic_sheep_item_class import *
 import field_resources
 
 class FieldGraphicsScene(QGraphicsScene):
-	"""this class provides a scene to manage items in the field"""
+    """this class provides a scene to manage items in the field"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		super().__init__()
-		
-		self.field = Field(max_crops,max_animals)
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        super().__init__()
+        
+        self.field = Field(max_crops,max_animals)
 
-		self.background_brush = QBrush()
-		self.background_picture = QPixmap(":/field_background.png")
-		self.background_brush.setTexture(self.background_picture)
-		self.setBackgroundBrush(self.background_brush)
+        self.background_brush = QBrush()
+        self.background_picture = QPixmap(":/field_background.png")
+        self.background_brush.setTexture(self.background_picture)
+        self.setBackgroundBrush(self.background_brush)
