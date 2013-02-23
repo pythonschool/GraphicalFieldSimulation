3c75187fe0f71350a2a79cfcb18381a9a2c4d8fb
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:22:39 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_drag_label_class.py
+++ graphic_drag_label_class.py
@@ -4,44 +4,44 @@ from PyQt4.QtCore import *
 import field_resources
 
 class QDragLabel(QLabel):
-	"""this class provides an image label that can be dragged and dropped"""
+    """this class provides an image label that can be dragged and dropped"""
 
-	#constructor
-	def __init__(self,picture):
-		super().__init__()
-		self.setPixmap(picture.scaledToWidth(35,1))
+    #constructor
+    def __init__(self,picture):
+        super().__init__()
+        self.setPixmap(picture.scaledToWidth(35,1))
 
-	def mouseMoveEvent(self,event):
-		#if the left mouse button is used
-		if event.buttons() == Qt.LeftButton:
-			data = QByteArray()
-			mime_data = QMimeData()
-			mime_data.setData(self.mimetext,data)
-			drag = QDrag(self)
-			drag.setMimeData(mime_data)
-			drag.setHotSpot(self.rect().topLeft()) #where do we drag from
-			drop_action = drag.start(Qt.MoveAction) #drag starts
+    def mouseMoveEvent(self,event):
+        #if the left mouse button is used
+        if event.buttons() == Qt.LeftButton:
+            data = QByteArray()
+            mime_data = QMimeData()
+            mime_data.setData(self.mimetext,data)
+            drag = QDrag(self)
+            drag.setMimeData(mime_data)
+            drag.setHotSpot(self.rect().topLeft()) #where do we drag from
+            drop_action = drag.start(Qt.MoveAction) #drag starts
 
 class WheatDragLabel(QDragLabel):
-	"""this class provides an wheat label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/wheat_seed.png"))
-		self.mimetext = "application/x-wheat"
+    """this class provides an wheat label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/wheat_seed.png"))
+        self.mimetext = "application/x-wheat"
 
 class PotatoDragLabel(QDragLabel):
-	"""this class provides an potato label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/potato_seed.png"))
-		self.mimetext = "application/x-potato"
+    """this class provides an potato label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/potato_seed.png"))
+        self.mimetext = "application/x-potato"
 
 class CowDragLabel(QDragLabel):
-	"""this class provides an cow label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/cow_baby.png"))
-		self.mimetext = "application/x-cow"
+    """this class provides an cow label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/cow_baby.png"))
+        self.mimetext = "application/x-cow"
 
 class SheepDragLabel(QDragLabel):
-	"""this class provides an sheep label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/sheep_baby.png"))
-		self.mimetext = "application/x-sheep"
+    """this class provides an sheep label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/sheep_baby.png"))
+        self.mimetext = "application/x-sheep"
--- graphic_wheat_item_class.py
+++ graphic_wheat_item_class.py
@@ -4,11 +4,11 @@ from wheat_class import *
 import field_resources
 
 class WheatGraphicsPixmapItem(CropGraphicsPixmapItem):
-	"""this class provides a graphical representation of a wheat"""
+    """this class provides a graphical representation of a wheat"""
 
-	#constructor
-	def __init__(self):
-		self.available_graphics = [":/wheat_seed.png", ":/wheat_seedling.png",":/wheat_young.png",":/wheat_mature.png",":/wheat_old.png"]
-		super().__init__(self.available_graphics)
+    #constructor
+    def __init__(self):
+        self.available_graphics = [":/wheat_seed.png", ":/wheat_seedling.png",":/wheat_young.png",":/wheat_mature.png",":/wheat_old.png"]
+        super().__init__(self.available_graphics)
 
-		self.crop = Wheat()
+        self.crop = Wheat()
