1b7db9d117b1981461c2e7678e3ddb6256cf0100
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:18:19 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_crop_item_class.py
+++ graphic_crop_item_class.py
@@ -1,22 +1,22 @@
 from graphic_field_item_class import *
 
 class CropGraphicsPixmapItem(FieldItemGraphicsPixmapItem):
-	"""this class provides a pixmap item with a preset image for the crop"""
+    """this class provides a pixmap item with a preset image for the crop"""
 
-	#constructor
-	def __init__(self, graphics_list):
-		super().__init__(graphics_list)
+    #constructor
+    def __init__(self, graphics_list):
+        super().__init__(graphics_list)
 
-		self.crop = None
+        self.crop = None
 
-	def update_status(self):
-		if self.crop._status == "Seed":
-			self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(25,1))
-		elif self.crop._status == "Seedling":
-			self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(25,1))
-		elif self.crop._status == "Young":
-			self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(25,1))
-		elif self.crop._status == "Mature":
-			self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(25,1))
-		elif self.crop._status == "Old":
-			self.setPixmap(QPixmap(self.available_graphics[4]).scaledToWidth(25,1))	
+    def update_status(self):
+        if self.crop._status == "Seed":
+            self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(25,1))
+        elif self.crop._status == "Seedling":
+            self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(25,1))
+        elif self.crop._status == "Young":
+            self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(25,1))
+        elif self.crop._status == "Mature":
+            self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(25,1))
+        elif self.crop._status == "Old":
+            self.setPixmap(QPixmap(self.available_graphics[4]).scaledToWidth(25,1)) 
--- graphic_field_scene_class.py
+++ graphic_field_scene_class.py
@@ -9,78 +9,78 @@ from graphic_sheep_item_class import *
 import field_resources
 
 class FieldGraphicsScene(QGraphicsScene):
-	"""this class provides a scene to manage items in the field"""
+    """this class provides a scene to manage items in the field"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		super().__init__()
-		
-		self.field = Field(max_crops,max_animals)
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        super().__init__()
+        
+        self.field = Field(max_crops,max_animals)
 
-		self.background_brush = QBrush()
-		self.background_picture = QPixmap(":/field_background.png")
-		self.background_brush.setTexture(self.background_picture)
-		self.setBackgroundBrush(self.background_brush)
+        self.background_brush = QBrush()
+        self.background_picture = QPixmap(":/field_background.png")
+        self.background_brush.setTexture(self.background_picture)
+        self.setBackgroundBrush(self.background_brush)
 
-	def _drop_position(self,item):
-		cursor_position = QCursor.pos() #global cursor position
-		current_view = self.views()[0]
-		scene_position = current_view.mapFromGlobal(cursor_position)
+    def _drop_position(self,item):
+        cursor_position = QCursor.pos() #global cursor position
+        current_view = self.views()[0]
+        scene_position = current_view.mapFromGlobal(cursor_position)
 
-		width = item.boundingRect().width()
-		height = item.boundingRect().height()
+        width = item.boundingRect().width()
+        height = item.boundingRect().height()
 
-		width_offset = width/2
-		height_offset = height/2
+        width_offset = width/2
+        height_offset = height/2
 
-		drop_x = scene_position.x() - width_offset
-		drop_y = scene_position.y() - height_offset
+        drop_x = scene_position.x() - width_offset
+        drop_y = scene_position.y() - height_offset
 
-		return drop_x, drop_y
+        return drop_x, drop_y
 
-	def _visualise_graphic_item(self,graphic_item_type):
-		if graphic_item_type == "crop":
-			x, y = self._drop_position(self.field._crops[-1])
-			self.field._crops[-1].setPos(x,y)
-			self.addItem(self.field._crops[-1])
-		elif graphic_item_type == "animal":
-			x, y = self._drop_position(self.field._animals[-1])
-			self.field._animals[-1].setPos(x,y)
-			self.addItem(self.field._animals[-1])
+    def _visualise_graphic_item(self,graphic_item_type):
+        if graphic_item_type == "crop":
+            x, y = self._drop_position(self.field._crops[-1])
+            self.field._crops[-1].setPos(x,y)
+            self.addItem(self.field._crops[-1])
+        elif graphic_item_type == "animal":
+            x, y = self._drop_position(self.field._animals[-1])
+            self.field._animals[-1].setPos(x,y)
+            self.addItem(self.field._animals[-1])
 
-	def _add_graphic_item(self,result,graphic_item_type):
-		if result:
-			self._visualise_graphic_item(graphic_item_type)
-		else:
-			error_message = QMessageBox()
-			error_message.setText("No more {0}s can be added to this field".format(graphic_item_type))
-			error_message.exec()
+    def _add_graphic_item(self,result,graphic_item_type):
+        if result:
+            self._visualise_graphic_item(graphic_item_type)
+        else:
+            error_message = QMessageBox()
+            error_message.setText("No more {0}s can be added to this field".format(graphic_item_type))
+            error_message.exec()
 
-	#this method overrides the parent method
-	def dragEnterEvent(self,event):
-		#what to do if an object is dragged into the scene
-		event.accept()
+    #this method overrides the parent method
+    def dragEnterEvent(self,event):
+        #what to do if an object is dragged into the scene
+        event.accept()
 
-	#this method overrides the parent method
-	def dragMoveEvent(self,event):
-		event.accept()
+    #this method overrides the parent method
+    def dragMoveEvent(self,event):
+        event.accept()
 
-	#this method overrides the parent method
-	def dropEvent(self,event):
-		event.accept()
+    #this method overrides the parent method
+    def dropEvent(self,event):
+        event.accept()
 
-		#what to do if an object is dropped on the scene
-		if event.mimeData().hasFormat("application/x-wheat"):
-			crop_added = self.field.plant_crop(WheatGraphicsPixmapItem())
-			self._add_graphic_item(crop_added,"crop")
-		elif event.mimeData().hasFormat("application/x-potato"):
-			crop_added = self.field.plant_crop(PotatoGraphicsPixmapItem())
-			self._add_graphic_item(crop_added,"crop")
-		elif event.mimeData().hasFormat("application/x-cow"):
-			animal_added = self.field.add_animal(CowGraphicsPixmapItem())
-			self._add_graphic_item(animal_added,"animal")
-		elif event.mimeData().hasFormat("application/x-sheep"):
-			animal_added = self.field.add_animal(SheepGraphicsPixmapItem())
-			self._add_graphic_item(animal_added,"animal")
-		else:
-			print("not recognised")
+        #what to do if an object is dropped on the scene
+        if event.mimeData().hasFormat("application/x-wheat"):
+            crop_added = self.field.plant_crop(WheatGraphicsPixmapItem())
+            self._add_graphic_item(crop_added,"crop")
+        elif event.mimeData().hasFormat("application/x-potato"):
+            crop_added = self.field.plant_crop(PotatoGraphicsPixmapItem())
+            self._add_graphic_item(crop_added,"crop")
+        elif event.mimeData().hasFormat("application/x-cow"):
+            animal_added = self.field.add_animal(CowGraphicsPixmapItem())
+            self._add_graphic_item(animal_added,"animal")
+        elif event.mimeData().hasFormat("application/x-sheep"):
+            animal_added = self.field.add_animal(SheepGraphicsPixmapItem())
+            self._add_graphic_item(animal_added,"animal")
+        else:
+            print("not recognised")
