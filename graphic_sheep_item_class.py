09b0d89e90c7e41d8a87cec4ca4990fd577283dc
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:04:07 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_sheep_item_class.py
+++ graphic_sheep_item_class.py
@@ -4,11 +4,11 @@ from sheep_class import *
 import field_resources
 
 class SheepGraphicsPixmapItem(AnimalGraphicsPixmapItem):
-	"""this class provides a graphical representation of a sheep"""
+    """this class provides a graphical representation of a sheep"""
 
-	#constructor
-	def __init__(self):
-		self.available_graphics = [":/sheep_baby.png", ":/sheep_poor.png",":/sheep_fine.png",":/sheep_prime.png"]
-		super().__init__(self.available_graphics)
+    #constructor
+    def __init__(self):
+        self.available_graphics = [":/sheep_baby.png", ":/sheep_poor.png",":/sheep_fine.png",":/sheep_prime.png"]
+        super().__init__(self.available_graphics)
 
-		self.animal = Sheep("")
+        self.animal = Sheep("")
