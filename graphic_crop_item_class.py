117791343ee89a0fa968b423b96fc3bb0c441d06
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:20:32 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_crop_item_class.py
+++ graphic_crop_item_class.py
@@ -1,35 +1,35 @@
 from graphic_field_item_class import *
 
 class CropGraphicsPixmapItem(FieldItemGraphicsPixmapItem):
-	"""this class provides a pixmap item with a preset image for the crop"""
+    """this class provides a pixmap item with a preset image for the crop"""
 
-	#constructor
-	def __init__(self, graphics_list):
-		super().__init__(graphics_list)
+    #constructor
+    def __init__(self, graphics_list):
+        super().__init__(graphics_list)
 
-		self.crop = None
+        self.crop = None
 
-	def update_status(self):
-		if self.crop._status == "Seed":
-			self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(25,1))
-		elif self.crop._status == "Seedling":
-			self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(25,1))
-		elif self.crop._status == "Young":
-			self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(25,1))
-		elif self.crop._status == "Mature":
-			self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(25,1))
-		elif self.crop._status == "Old":
-			self.setPixmap(QPixmap(self.available_graphics[4]).scaledToWidth(25,1))	
+    def update_status(self):
+        if self.crop._status == "Seed":
+            self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(25,1))
+        elif self.crop._status == "Seedling":
+            self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(25,1))
+        elif self.crop._status == "Young":
+            self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(25,1))
+        elif self.crop._status == "Mature":
+            self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(25,1))
+        elif self.crop._status == "Old":
+            self.setPixmap(QPixmap(self.available_graphics[4]).scaledToWidth(25,1)) 
 
-	def _harvest_crop(self):
-		self.scene().harvest_crop(self)
+    def _harvest_crop(self):
+        self.scene().harvest_crop(self)
 
-	def contextMenuEvent(self,event):
-		menu = QMenu("Crop")
-		remove = menu.addAction("Harvest Crop")
+    def contextMenuEvent(self,event):
+        menu = QMenu("Crop")
+        remove = menu.addAction("Harvest Crop")
 
-		#connection
-		remove.triggered.connect(self._harvest_crop)
+        #connection
+        remove.triggered.connect(self._harvest_crop)
 
-		#run menu
-		menu.exec_(event.screenPos())
+        #run menu
+        menu.exec_(event.screenPos())
