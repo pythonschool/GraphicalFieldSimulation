5ecead898275d4c72645c9ace21d0b32a93c608f
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:12:11 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_drag_label_class.py
+++ graphic_drag_label_class.py
@@ -3,29 +3,29 @@ from PyQt4.QtGui import QLabel, QPixmap
 import field_resources
 
 class QDragLabel(QLabel):
-	"""this class provides an image label that can be dragged and dropped"""
+    """this class provides an image label that can be dragged and dropped"""
 
-	#constructor
-	def __init__(self,picture):
-		super().__init__()
-		self.setPixmap(picture.scaledToWidth(35,1))
+    #constructor
+    def __init__(self,picture):
+        super().__init__()
+        self.setPixmap(picture.scaledToWidth(35,1))
 
 class WheatDragLabel(QDragLabel):
-	"""this class provides an wheat label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/wheat_seed.png"))
+    """this class provides an wheat label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/wheat_seed.png"))
 
 class PotatoDragLabel(QDragLabel):
-	"""this class provides an potato label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/potato_seed.png"))
+    """this class provides an potato label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/potato_seed.png"))
 
 class CowDragLabel(QDragLabel):
-	"""this class provides an cow label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/cow_baby.png"))
+    """this class provides an cow label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/cow_baby.png"))
 
 class SheepDragLabel(QDragLabel):
-	"""this class provides an sheep label that can be dragged and dropped"""
-	def __init__(self):
-		super().__init__(QPixmap(":/sheep_baby.png"))
+    """this class provides an sheep label that can be dragged and dropped"""
+    def __init__(self):
+        super().__init__(QPixmap(":/sheep_baby.png"))
