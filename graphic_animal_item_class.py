009e3f4e0150f977e0af8ae2f1e4aea3322b77b7
Adam McNicol <adammcnicol@mac.com>
2013-02-23 18:08:05 +0000

    indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- graphic_animal_item_class.py
+++ graphic_animal_item_class.py
@@ -1,21 +1,21 @@
 from graphic_field_item_class import *
 
 class AnimalGraphicsPixmapItem(FieldItemGraphicsPixmapItem):
-	"""this class provides a pixmap item with a preset image for the animal"""
+    """this class provides a pixmap item with a preset image for the animal"""
 
-	#constructor
-	def __init__(self, graphics_list):
-		super().__init__(graphics_list)
-		self.setPixmap(self.current_graphic.scaledToWidth(50,1))
+    #constructor
+    def __init__(self, graphics_list):
+        super().__init__(graphics_list)
+        self.setPixmap(self.current_graphic.scaledToWidth(50,1))
 
-		self.animal = None
+        self.animal = None
 
-	def update_status(self):
-		if self.animal._status == "Baby":
-			self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(50,1))
-		elif self.animal._status == "Poor":
-			self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(50,1))
-		elif self.animal._status == "Fine":
-			self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(50,1))
-		elif self.animal._status == "Prime":
-			self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(50,1))
+    def update_status(self):
+        if self.animal._status == "Baby":
+            self.setPixmap(QPixmap(self.available_graphics[0]).scaledToWidth(50,1))
+        elif self.animal._status == "Poor":
+            self.setPixmap(QPixmap(self.available_graphics[1]).scaledToWidth(50,1))
+        elif self.animal._status == "Fine":
+            self.setPixmap(QPixmap(self.available_graphics[2]).scaledToWidth(50,1))
+        elif self.animal._status == "Prime":
+            self.setPixmap(QPixmap(self.available_graphics[3]).scaledToWidth(50,1))
